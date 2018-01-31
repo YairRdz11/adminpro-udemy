91a3d45f2e4bb240ceb9b1ff3d556823d0dc6b77
Yair <francisco.rodriguez@ipicyt.edu.mx>
2018-01-31 16:26:00 -0600

    Primer commit

---
+++ src/app/shared/nopagefound/npagefound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-npagefound',
+  templateUrl: './npagefound.component.html',
+  styles: []
+})
+export class NpagefoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
